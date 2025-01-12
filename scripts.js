f27fd3f75bfdfa78e8e87c40a8536547621059f1
Núbia <devnubia@gmail.com>
2025-01-11 21:21:04 -0300

    Store Coffee

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara-modal")
+
+function showModal() {
+  modal.style.left = '50%'
+  mascara.style.visibility = 'visible'
+  
+}
+
+function closeModal() {
+  modal.style.left = '-30%'
+  mascara.style.visibility = 'hidden'
+}
